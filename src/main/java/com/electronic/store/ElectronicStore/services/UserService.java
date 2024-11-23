c09703a09f136ac59d0958b9d3e0417cb4269f44
Chandan Agrawal <cagrawal0012@gmail.com>
2024-11-23 19:24:24 +0530

    UserService added

---
+++ src/main/java/com/electronic/store/ElectronicStore/services/UserService.java
@@ -0,0 +1,12 @@
+package com.electronic.store.ElectronicStore.services;
+
+public interface UserService {
+
+    //create
+    //update
+    //delete
+    //get all users
+    //get single user by id
+    //get single user by email
+    //search user
+}
---
+++ src/main/java/com/electronic/store/ElectronicStore/services/impl/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.electronic.store.ElectronicStore.services.impl;
+
+import com.electronic.store.ElectronicStore.services.UserService;
+
+public class UserServiceImpl implements UserService {
+
+    //create
+    //update
+    //delete
+    //get all users
+    //get single user by id
+    //get single user by email
+    //search user
+}
